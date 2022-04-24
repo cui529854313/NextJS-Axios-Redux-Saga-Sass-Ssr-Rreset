c9b8c4a49b8f8ff0e8762e50d9d5c807bd1f20e9
聂港 <75252598+dingdangpocket@users.noreply.github.com>
2022-04-25 07:56:37 +0800

    Update index.js

--- redux/reducers/index.js
+++ redux/reducers/index.js
@@ -3,7 +3,6 @@ import countReducer from "./countReducer"
 import userReducer from "./userReducer"
 import modelReducer from "./modelReducer"
 
-// 将多个reducer合并位一个reducer
 export default combineReducers({
     counts:countReducer,
     user:userReducer,
